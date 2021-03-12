28f9f4731b26d988668b760f959c57f610dce7aa
CoultonF <cjrfraser@gmail.com>
2021-03-12 08:26:53 +0000

    barebones ui for react implemented.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import * as PropTypes from 'prop-types'
 import Button from './Button'
+import SearchBar from './SearchBar'
 
 const Header = ({title}) => {
+
+    const [input, setInput] = useState('');
+    
+    const updateInput = async (input) => {
+        setInput(input);
+    }
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Search'></Button>
+            <SearchBar input={input} onChange={updateInput}></SearchBar>
         </header>
     )
 }
 
 Header.defaultProps = {
-    title: 'Carin'
+    title: 'Cairn'
 }
 
 Header.propType = {
